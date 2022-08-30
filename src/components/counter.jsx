01c6f93e472e10d69c5729fd3d005616b95c6c66
Sergio Nieto <snieto10@gmail.com>
2022-08-30 10:01:44 -0400

    add expression

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    state = { 
+        count: 0
+    };
     render() { 
-        return <h1>Hello World</h1>;
+        return (<div>
+        <span>{this.formatCount()}</span><button>Increment</button>
+                </div>);
     }
+    
+    formatCount() {
+        const { count } = this.state
+        return count === 0 ? <h1>Zero</h1> : count;
+    }
+
 }
  
 export default Counter;
