6b98c833fc8b08ccd0b6e7da8cbf240ecd544c11
Sergio Nieto <snieto10@gmail.com>
2022-08-30 18:08:55 -0400

    add bootstrap

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
-        count: 0
+        count: 0,
     };
     render() { 
-        return (<div>
-        <span>{this.formatCount()}</span><button>Increment</button>
-                </div>);
+        return (
+        <div>
+            <span className="badge badge-primary m-2">{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm">Increment</button>
+        </div>);
     }
     
     formatCount() {
