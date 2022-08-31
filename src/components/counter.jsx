1a7aeb87f02089363aa44a596dae8b702f3e255d
Sergio Nieto <snieto10@gmail.com>
2022-08-30 21:17:43 -0400

    step 6

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {}
+    state = {
+        count: 0
+    };
     render(){
         return (
         <div>
-            <h1> Hello World</h1>
-            <button>Increment</button>
+            <span className="badge badge-primary m-2"> {this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm">Increment</button>
         </div>
         )
     }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
+
 }
 
 export default Counter
