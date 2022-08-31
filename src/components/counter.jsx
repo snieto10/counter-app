3db492790600b84c61a6e64f5bd1f34679dd2c84
Sergio Nieto <snieto10@gmail.com>
2022-08-31 08:33:10 -0400

    step 9

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,8 +3,17 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
-        tags: ['tag1', 'tag2', 'tag3']
+        tags: ['tag1','tag2', 'tag3']
     };
+
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+        return <ul>
+        { this.state.tags.map (tag => <li key={tag}>{tag}</li>) }
+    </ul>
+    }
+
     render(){
         let classes = this.getBadgeClasses();
         
@@ -12,9 +21,8 @@ class Counter extends Component {
         <div>
             <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
-            <ul>
-                { this.state.tags.map (tag => <li key={tag}>{tag}</li>) }
-            </ul>
+            <section>{(this.state.tags.length === 0) && 'Please create a new Tag!' }</section>
+            {this.renderTags()}
         </div>
         )
     }
