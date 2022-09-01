c920f1ba3237634eafb5f1cdec9937557802c4c7
Sergio Nieto <snieto10@gmail.com>
2022-09-01 13:51:52 -0400

    sec 4 step 11

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Counters from "./components/counters";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Counters />
+    <App />
   </React.StrictMode>
 );
 
